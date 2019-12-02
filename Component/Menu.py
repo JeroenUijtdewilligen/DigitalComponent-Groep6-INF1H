6092a6378534dc5a896ef624fc1dccc8e8955510
GVisser1 <55433031+GVisser1@users.noreply.github.com>
2019-12-02 11:05:18 +0100

    Tutorial verbeterd
    Knop om terug naar het menu te gaan / naar de 1e pagina. Als je met de muis op de knop zit, wordt deze lichter van kleur.
    Handleiding geupdate.

--- Component/Menu.py
+++ Component/Menu.py
@@ -54,7 +54,7 @@ def draw():
     textAlign(CENTER)
     text("Spel Starten", 235, 310)
     text("Repeat Hulp", 235, 460)
-    text("Handleiding", 235, 610)
+    text("Tutorial", 235, 610)
     
 def mousePressed():
     global scene
